2e1a74a73527b13e7b24bd680846c2d562dec3cd
bass.alana <bass.alana@vashonsd.org>
2021-10-06 10:41:50 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        System.out.println(squareOf(23));
+    }
+
+    public static int squareOf(int num) {
+        return num*num;
+    }
+
+}
